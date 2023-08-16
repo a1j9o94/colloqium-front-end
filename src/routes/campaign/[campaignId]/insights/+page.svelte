283206542c68a24cf185ef41bdb5c6d84f744377
Adrian Obleton <obletonadrian@gmail.com>
2023-08-16 17:11:05 -0400

    feat: Basic insights page without async loading or formatting

--- src/routes/campaign/[campaignId]/insights/+page.svelte
+++ src/routes/campaign/[campaignId]/insights/+page.svelte
@@ -3,11 +3,19 @@
 	import type { Interaction } from '$lib/model.js';
     import { API_URL } from '$lib/utility';
 	import { onMount } from 'svelte';
+    import 'c3/c3.css';
     
     export let data;
     const { campaign } = data;
     let interactions: Interaction[];
     let evaluated_interactions: Interaction[] = [];
+    let summaries = [
+        { title: 'Campaign Manager Summary', content: campaign.campaign_manager_summary },
+        { title: 'Communications Summary', content: campaign.communications_director_summary },
+        { title: 'Field Summary', content: campaign.field_director_summary }
+    ];
+    let carousel;
+    import { browser } from '$app/environment';
 
     async function refreshEvaluations() {
         const res = await fetch(`${API_URL}/campaign/insights`, {
@@ -60,62 +68,113 @@
         console.log(data);
         interactions = data.interactions;
 
+        console.log(summaries)
+
         // Filter for interactions where campaign_relevance_score is not null
         evaluated_interactions = interactions.filter(interaction => interaction.campaign_relevance_score != null);
+        evaluated_interactions.sort((a, b) => b.campaign_relevance_score - a.campaign_relevance_score);
+
+
+        const object = await import('c3');
+        const c3 = object.default;
+
+        var chart = c3.generate({
+            bindto: '#funnelChart', // make sure to bind to an element with this ID
+            data: {
+                columns: [
+                    ['Sent', campaign.interactions_sent],
+                    ['Delivered', campaign.interactions_delivered],
+                    ['Responded', campaign.interactions_responded],
+                    ['Converted', campaign.interactions_converted]
+                ],
+                type: 'bar'
+            },
+            bar: {
+                width: {
+                    ratio: 0.5
+                }
+            }
+        });
     });
 </script>
 
 <AuthCheck>
     <!-- Three buttons for "Refresh Evaluations", "Refresh Funnel", "Refresh Insights"-->
     
-    <div>
+    <div id="funnelChart-container">
     <h1>Funnel</h1>
-    <p>Sent: {campaign.interactions_sent}</p>
-    <p>Delivered: {campaign.interactions_delivered}</p>
-    <p>Responded: {campaign.interactions_responded}</p>
-    <p>Converted: {campaign.interactions_converted}</p>
+    <div id="funnelChart"></div>
     <btn class="btn btn-primary" on:click={refreshFunnel()}>Refresh Funnel</btn>
     </div>
 
-    <div>
-        <h1>Campaign Summaries</h1>
-        <h2>Campaign Manager Summarry</h2>
-        <p>{campaign.campaign_manager_summary}</p>
-        <h2>Communications Summary</h2>
-        <p>{campaign.communications_director_summary}</p>
-        <h2>Field Summary</h2>
-        <p>{campaign.field_director_summary}</p>
-        <!-- Check if campaign.policy_insights is a string. If so, do not show it-->
+    <div class="p-20">
+            <h1 class="text-lg font-bold mb-2">Campaign Summaries</h1>
+                {#each summaries as summary}
+                <div class="mb-4">
+                    <h2 class="text-md font-semibold">{summary.title}</h2>
+                    <p class="text-sm">{summary.content}</p>
+                </div>
+                {/each}
+    
+        <!-- Check if campaign.policy_insights is a string. If so, do not show it -->
         {#if campaign.policy_insights && typeof campaign.policy_insights === 'object' && !(campaign.policy_insights instanceof String)}
-            <h2>Policy Insights</h2>
-
-            {#each Object.entries(campaign.policy_insights) as [policy_area, insight] }
-                <p>{policy_area}: {insight}</p>
+        <div class="mt-6">
+            <h2 class="text-lg font-bold mb-2">Policy Insights</h2>
+            {#each Object.entries(campaign.policy_insights) as [policy_area, insight]}
+            <p class="text-md"><span class="font-semibold">{policy_area}:</span> {insight}</p>
             {/each}
+        </div>
         {/if}
-        <btn class="btn btn-primary" on:click={refreshInsights}>Refresh Insights</btn>
+    
+        <button class="btn btn-primary mt-4" on:click={refreshInsights}>Refresh Insights</button>
     </div>
+    
 
     
-    <btn class="btn btn-primary" on:click={refreshEvaluations}>Refresh Evaluations</btn>
-    {#each evaluated_interactions as interaction}
-        <div class="card bg-base-100">
-            <div class="card-body">
-                <h2>Campaign Relevance Score: {interaction.campaign_relevance_score}</h2>
-                <p>{interaction.campaign_relevance_summary}</p>
-                {#if interaction.insights_about_issues && typeof interaction.insights_about_issues === 'object' && !(interaction.insights_about_issues instanceof String)}
-                    <h2>Policy Insights</h2>
+    <button class="btn btn-primary mb-4" on:click={refreshEvaluations}>Refresh Evaluations</button>
+    <table class="min-w-full bg-base-100 text-base">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">Campaign Relevance Score</th>
+                <th class="px-4 py-2">Relevance Summary</th>
+                <th class="px-4 py-2">Policy Insights</th>
+                <th class="px-4 py-2">Voter Insights</th>
+                <th class="px-4 py-2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each evaluated_interactions as interaction}
+            <tr>
+                <td class="px-4 py-2">{interaction.campaign_relevance_score}</td>
+                <td class="px-4 py-2">{interaction.campaign_relevance_summary}</td>
+                <td class="px-4 py-2">
+                    {#if interaction.insights_about_issues && typeof interaction.insights_about_issues === 'object' && !(interaction.insights_about_issues instanceof String)}
+                        {#each Object.entries(interaction.insights_about_issues) as [policy_area, insight]}
+                            <p>{policy_area}: {insight}</p>
+                        {/each}
+                    {:else}
+                        No Policy Insights
+                    {/if}
+                </td>
+                <td class="px-4 py-2">{interaction.insights_about_voter}</td>
+                <td class="px-4 py-2"><a href="/interaction/{interaction.id}">View Interaction</a></td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
 
-                    {#each Object.entries(interaction.insights_about_issues) as [policy_area, insight] }
-                        <p>{policy_area}: {insight}</p>
-                    {/each}
-                {:else}
-                    <h2>No Policy Insights</h2>
-                {/if}
-                <p>{interaction.insights_about_voter}</p>
-                <a href="/interaction/{interaction.id}" >View Interaction</a>
-            </div>
-        </div>
-    {/each}
 
 </AuthCheck>
+
+<style>
+    #funnelChart-container {
+        width: 100%; /* Ensure the parent container takes the full available width */
+    }
+
+    #funnelChart {
+        width: 66.67%; /* 2/3rds of the parent container's width */
+        margin: 0 auto; /* Centering the chart within its parent container */
+    }
+
+
+</style>
